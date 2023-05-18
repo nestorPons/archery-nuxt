5cb1cb73c4066cc9de504f21c9b754cc7dbb09ea
nestorpons <nestorpons@gmail.com>
2023-05-18 13:12:07 +0200

    Opciones para tipo de arco

--- components/ArrowForm.vue
+++ components/ArrowForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center">
         <h2 class="text-xl uppercase font-bold mb-4 pb-10 text-center">Formulario de Selección de Flechas</h2>
-        <form @submit.prevent="submitForm" class="w-1/3">
+        <form @submit.prevent="submitForm" class="">
             <div class="component">
                 <label for="edad" class="mb-2">Edad:</label>
                 <input type="number" v-model="edad" required
@@ -23,40 +23,60 @@
                     <transition>
                         <img v-if="showHelp" src="~/assets/medicion.png" />
                     </transition>
-
                 </div>
-                <input type="number" v-model="longitud" required
+                <input type="number" v-model="longitudes" required
                     class="w-full border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <div class="sel-width">
+                <div class="sel-width radio-options">
                     <label>
-                        <input type="radio" name="unit-width" value="0"  v-model="lenghtUnit">
-                        <span>Centímetros</span>
+                        <input id="unit0" type="radio" name="unit-width" value="0" v-model="lenghtUnit">
+                        <span for="unit0">Centímetros</span>
                     </label>
                     <label>
-                        <input type="radio" name="unit-width" value="1" v-model="lenghtUnit">
-                        <span>Puldadas</span>
+                        <input id="unit1" type="radio" name="unit-width" value="1" v-model="lenghtUnit">
+                        <span for="unit1">Puldadas</span>
                     </label>
-                    <p>
-                        <span>
-                            {{ textChangeUnit }}.
-                        </span>
-                    </p>
+                    <p>{{ textChangeUnit }}</p>
                 </div>
 
+            </div>
+            <div class="component">
+                <label for="potencia" class="mb-2">Potencia en libras:</label>
+                <input type="number" v-model="potencia" required
+                    class="w-full border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="component">
                 <label for="tipoArco" class="mb-2">Tipo de Arco:</label>
                 <select v-model="tipoArco" required
                     class="w-full border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
                     <option value="">Seleccionar</option>
-                    <option value="Recurvo">Recurvo</option>
-                    <option value="Compuesto">Compuesto</option>
+                    <option value="1">Recurvo</option>
+                    <option value="2">Compuesto</option>
                 </select>
-            </div>
-            <div class="component">
-                <label for="potencia" class="mb-2">Peso de Dibujo del Arco:</label>
-                <input type="number" v-model="potencia" required
-                    class="w-full border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <div v-if="tipoArco==1">
+                    <label>
+                        <span>Apertura en pulgadas:</span>
+                        <input type="number" v-model="apertura"
+                            class="w-full border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    </label>
+                </div>
+                <div v-if="tipoArco==2">
+                    <label>
+                        <span>Suelta</span>
+                        <select v-model="suelta"
+                            class="w-full border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                            <option value="">Seleccionar</option>
+                            <option value="1">Con los dedos</option>
+                            <option value="2">Con disparador</option>
+                        </select>
+                    </label>
+                    <label>Peso de la punta</label>
+                    <input type="number" v-model="pesoPunta"
+                        class="w-full border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+
+                    <label>Fismele</label>
+                    <input type="number" v-model="fismele"
+                        class="w-full border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                </div>
             </div>
 
         </form>
@@ -65,7 +85,7 @@
         <p>Longitud: {{ longitud }}</p>
         <p>Unidad de longitud: {{ lenghtUnit }}</p>
         <p>Tipo de arco: {{ tipoArco }}</p>
-        <p>Potencia: {{ potencia }}</p>
+        <p>Potencia: {{ potenciaTotal }}</p>
         <div v-if="flechaRecomendada" class="mt-8">
             <h3 class="text-xl font-bold mb-2">Flecha Recomendada:</h3>
             <p>{{ flechaRecomendada }}</p>
@@ -75,19 +95,56 @@
   
 <script setup>
   import { ref } from 'vue';
-  const edad = ref(null);
-  const longitud = ref(72);
-  const tipoArco = ref('');
-  const potencia = ref(null);
-  const flechaRecomendada = ref(null);
+  const long = reactive({
+    in: 28.35, 
+    cm: 72
+  })
+  const edad = ref(30);
+  const longitudes = computed({
+    get(){
+        return lenghtUnit.value == 1 ? long.in : long.cm
+    },
+    set(value){
+        if(lenghtUnit.value == 1 ){
+            // Pulgadas
+            long.in = value; 
+            long.cm = (value * 2.54).toFixed(2)
+            long.value = value
+        }else{
+            // Centimetros
+            long.in = (value / 2.54).toFixed(2)
+            long.cm = value; 
+            long.value = value
+        }
+        
+    }
+  });
+  const tipoArco = ref(1);
+  const potencia = ref(30);
   const showHelp = ref(false);
   const lenghtUnit = ref(0);
-  const textChangeUnit = computed(() => {
-    return lenghtUnit.value == 1 ? 
-        `${(longitud.value * 2.54).toFixed(2)} centimetros` : 
-        `${(longitud.value / 2.54).toFixed(2)} pulgadas` ;
+  const suelta = ref(1);
+  const pesoPunta = ref(100);
+  const fismele = ref(6.5);
+  const flechaRecomendada = ref(null);
+  const textChangeUnit = computed(() => 
+    lenghtUnit.value == 1 ? 
+        `${long.cm} centímetros` : 
+        `${long.in} pulgadas` 
+  )    
+  const potenciaTotal = computed(()=>{
+    if (tipoArco.value==2 ){
+        // Suplemento de 5lbs si la suelta es con los dedos
+        const supSuelta =  suelta.value == 1 ? 5 : 0;
+        // Suplemento de 3lbd por cada 25 grains a partir de 100 grains 
+        const supPesoPunta = Math.ceil((pesoPunta.value - 100) / 25) * 3 ;
+        const supFismele = fismele.value < 6.5 ? 5 : 0 ;
+        return potencia.value + supSuelta + supPesoPunta + supFismele
+    }else{
+        return potencia
+    }
   })
-
+  const apertura = ref(long.in);
   function submitForm(event) {
     // Aquí puedes realizar la lógica para calcular la flecha recomendada
     // basada en los valores del formulario y asignarla a la propiedad flechaRecomendada
@@ -96,13 +153,41 @@
   
 <style lang="less" scoped>
 .component {
-    min-width: 350px;
+    width: 350px;
     margin-bottom: 1rem;
+
+    h3 {
+        font-size: 1.2rem;
+        text-transform: uppercase;
+        width: 100%;
+        margin-bottom: .5rem;
+    }
 
     input,
     select {
         margin-top: .3rem;
     }
+
+    .radio-options {
+        label {
+            cursor: pointer;
+        }
+
+        input {
+            margin-right: 1rem;
+            height: 1rem;
+            width: 1rem;
+            font-size: 3rem;
+        }
+    }
+
+    input,
+    select {
+        border: 1px;
+        padding: 1rem;
+        border: 1px solid gray;
+    }
+
 }
 
 .v-enter-from {
@@ -136,12 +221,6 @@
     padding: 1rem;
 }
 
-input,
-select {
-    border: 1px;
-    padding: 1rem;
-    border: 1px solid orangered;
-}
 
 .width-arrow {
     display: flex;
@@ -182,25 +261,16 @@ select {
     }
 }
 
+
 .sel-width {
     display: flex;
     flex-wrap: wrap;
-    justify-content:space-evenly;
-    align-items: flex-end;
+    justify-content: space-evenly;
     flex-direction: row;
     width: 100%;
-    label {
-        cursor: pointer;
-        width: 30%;
-        input {
-            margin-right: 1rem;
-            height: 1rem;
-            width: 1rem;
-            color: red;
-            fill: red;
-            border: 1px solid red;
-            font-size: 3rem;
-        }
+
+    p {
+        width: 100%;
     }
 }
 </style>
