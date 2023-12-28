a7b2937061e5b85c28519cdc4d83be315224cc55
nestorpons <nestorpons@gmail.com>
2023-12-28 13:19:25 +0100

    first deploy

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,20 +30,20 @@
                         <span class="absolute -inset-0.5"></span>
                         <span class="sr-only">Open main menu</span>
                         <!--
-            Icon when menu is closed.
+                            Icon when menu is closed.
 
-            Menu open: "hidden", Menu closed: "block"
-          -->
+                            Menu open: "hidden", Menu closed: "block"
+                        -->
                         <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                         <!--
-            Icon when menu is open.
+                            Icon when menu is open.
 
-            Menu open: "block", Menu closed: "hidden"
-          -->
+                            Menu open: "block", Menu closed: "hidden"
+                        -->
                         <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -52,17 +52,11 @@
                 </div>
                 <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="h-22 w-auto" src="/img/logo-light.png" alt="Your Company">
+                        <img class="h-15 w-auto" src="/img/logo-light.png" alt="Your Company">
                     </div>
 
                     <div class="hidden sm:ml-6 sm:block sm:flex md:flex lg:flex">
                         <div class="m-auto ">
-                            <!-- Current: "bg-cyan-950 text-white", Default: "text-blue-300 hover:bg-cyan-700 hover:text-white" -->
-                            <NuxtLink to="/" class="bg-cyan-950 text-white rounded-md px-3 py-2 text-sm font-medium"
-                                aria-current="page" 
-                                @click="closeMobileMenu">
-                                {{ $text('Home') }}
-                            </NuxtLink>
                             <NuxtLink to="/featuresForm"
                                 class="text-blue-300 hover:bg-cyan-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                                 @click="closeMobileMenu">
@@ -90,12 +84,6 @@
             <!-- Mobile menu, show/hide based on menu state. -->
             <div class="sm:hidden" id="mobile-menu" v-show="isMobileMenuOpen">
                 <div class="space-y-1 px-2 pb-3 pt-2">
-                    <NuxtLink to="/" 
-                        class="bg-cyan-950 text-white block rounded-md px-3 py-2 text-base font-medium"
-                        aria-current="page" 
-                        @click="closeMobileMenu">
-                        {{ $text('Home') }}
-                    </NuxtLink>
                     <NuxtLink to="/featuresForm"
                         class="bg-cyan-950 text-white block rounded-md px-3 py-2 text-base font-medium" 
                         aria-current="page"
